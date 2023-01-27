20a48f0819475fc2886d40672b868b9376134db7
Kashi639 <sawantsohan555@gmail.com>
2023-01-27 13:58:20 +0530

    added environment variables into .env file

--- server/utils/database.js
+++ server/utils/database.js
@@ -1,8 +1,12 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize('workoutdb', 'root', 'outlast2k15', {
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     dialect: 'mysql',
-    host: 'localhost', 
+    host: process.env.DB_HOST, 
 });
 
 export default sequelize;
